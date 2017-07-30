62b22d5f57f91b9bedd6fd6659164e62df3b09bd
Mark Healy <Mark Healy>
2017-07-30 23:18:29 +0100

    Added angular material

--- platform/client/src/app/app.module.ts
+++ platform/client/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }    from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
- 
-import { AppComponent }  from './app.component';
-import { routing }        from './routing';
- 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AppComponent } from './app.component';
+import { routing } from './routing';
+import { MaterialModule } from '@angular/material';
+import {Hammerjs } from 'hammerjs';
+import { MdButtonModule, MdCheckboxModule, MdMenuModule } from '@angular/material';
 import { AlertComponent } from './alerts/alert.component';
 import { AuthGuard } from './auth/auth.guard';
 import { AlertService} from './alerts/alert.service';
@@ -15,13 +17,19 @@ import { ProfileService } from './profile/profile.service';
 import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './auth/auth.login.component';
 import { RegisterComponent } from './auth/auth.register.component';
- 
+
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
-        routing
+        routing,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        MaterialModule,
+        MdButtonModule,
+        MdCheckboxModule,
+        MdMenuModule
     ],
     declarations: [
         AppComponent,
